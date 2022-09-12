ec080ff29cdc7eb5d4676044075739115ce2fc49
Gongzai Li <gli65@uclive.ac.nz>
2022-09-12 21:04:49 +1200

    issue: Cryptographic Failure. It was using unsalt md5 password hasher, Now is BCryptSHA256PasswordHasher

--- core/settings.py
+++ core/settings.py
@@ -67,7 +67,11 @@ MIDDLEWARE = [
 ]
 
 PASSWORD_HASHERS = [
-    "django.contrib.auth.hashers.UnsaltedMD5PasswordHasher",
+    # "django.contrib.auth.hashers.UnsaltedMD5PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
 ]
 
 ROOT_URLCONF = "core.urls"
