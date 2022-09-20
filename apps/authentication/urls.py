f7b11e3a7ba2018ff123c21e48f20f26ba44c79a
Gongzai Li <gli65@uclive.ac.nz>
2022-09-20 22:50:06 +1200

    fix: 7.2 Identification and Authentication Factures -> reset password by email.

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -4,7 +4,8 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.urls import path
-from .views import login_user, register_user, reset_password, register_activate
+from .views import login_user, register_user, reset_password, register_activate, reset_password_done, \
+    reset_password_sent, reset_password_confirm
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
@@ -13,4 +14,7 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout"),
     path("password/", reset_password, name="password"),
     path("activate/<uidb64>/<token>/", register_activate, name='register_activate'),
+    path("reset_password/sent/", reset_password_sent, name="reset_password_sent"),
+    path("reset/<uidb64>/<token>/", reset_password_confirm, name="reset_password_confirm"),
+    path("reset_password/done/", reset_password_done, name="reset_password_done")
 ]
