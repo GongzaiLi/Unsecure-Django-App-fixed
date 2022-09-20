3cdffa6aaf39470532fa6800fe7a1c2446a350b5
Gongzai Li <gli65@uclive.ac.nz>
2022-09-20 12:38:09 +1200

    fix: Injection - fixing upload py file in image, now when user

--- apps/home/models.py
+++ apps/home/models.py
@@ -9,6 +9,17 @@ from django.contrib.auth.models import User
 ##
 ## Helper functions
 ##
+def validate_image_file_extension(value):
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
+    valid_extensions = ['.blp', '.bmp', '.dib', '.bufr', '.cur', '.pcx', '.dcx', '.dds', '.ps', '.eps', '.fit', '.fits',
+                        '.fli', '.flc', '.ftc', '.ftu', '.gbr', '.gif', '.grib', '.h5', '.hdf', '.png', '.apng', '.jp2',
+                        '.j2k', '.jpc', '.jpf', '.jpx', '.j2c', '.icns', '.ico', '.im', '.iim', '.tif', '.tiff',
+                        '.jfif', '.jpe', '.jpg', '.jpeg', '.mpg', '.mpeg', '.mpo', '.msp', '.palm', '.pcd', '.pdf',
+                        '.pxr', '.pbm', '.pgm', '.ppm', '.pnm', '.psd', '.bw', '.rgb', '.rgba', '.sgi', '.ras', '.tga',
+                        '.icb', '.vda', '.vst', '.webp', '.wmf', '.emf', '.xbm', '.xpm']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError(F'file type {ext} not allow!')
 
 # Overwrite existing files for bio files
 # adapted from https://gist.github.com/fabiomontefuscolo/1584462
@@ -28,7 +39,8 @@ class UserProfile(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     bio_file = models.FileField(upload_to="bio", blank=True, null=True, storage=OverwriteStorage())
-    picture_file = models.FileField(upload_to="profile_pics", blank=True, null=True)
+    picture_file = models.FileField(upload_to="profile_pics", blank=True, null=True, storage=OverwriteStorage(),
+                                    validators=[validate_image_file_extension])
 
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name
@@ -37,7 +49,8 @@ class UserProfile(models.Model):
 class Project(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, unique=True)
-    picture_file = models.FileField(upload_to="project_pics", blank=True, null=True)
+    picture_file = models.FileField(upload_to="project_pics", blank=True, null=True, storage=OverwriteStorage(),
+                                    validators=[validate_image_file_extension])
     budget = models.FloatField(validators=[MinValueValidator(0)])
     completion = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     members = models.ManyToManyField(UserProfile)
