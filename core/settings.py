615c85d48e77096adc5aebd64baaad19b0c82c17
Gongzai Li <gli65@uclive.ac.nz>
2022-09-21 00:32:39 +1200

    fix: 7.1 now the password should more than 10 and no similarity , and check common or not

--- core/settings.py
+++ core/settings.py
@@ -132,7 +132,25 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        # 'OPTIONS': {
+        #     'user_attributes': (
+        #         'username', 'email', 'first_name', 'last_name'
+        #     ),
+        #     'max_similarity': 0.5
+        # }
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 10,
+        }
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
@@ -151,7 +169,6 @@ STATIC_URL = "/static/"
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (os.path.join(CORE_DIR, "apps/static"),)
-
 
 # email settings
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
